300da9dc524faae325b5e90a950f5d77f83c9c53
haondt <burghardtnoah@gmail.com>
2020-08-20 10:41:28 -0600

    replaced nested loops with recursive function

--- Lock Generator.py
+++ Lock Generator.py
@@ -1,29 +1,58 @@
-Dictionary={}
-with open("Dictionary.txt") as Dictionary_File:
-    for word in Dictionary_File:
-        #Number of character dials
-        if len(word.strip())==4:
-            Dictionary[word.strip().upper()]= True
+# All letters available
+letters = [
+    'cgflbptsmd',
+    'earthiuoyl',
+    'ealtoinsrm',
+    'plyamdkesx'
+]
 
-#All letters available
-a='cgflbptsmd'
-b='earthiuoyl'
-c='ealtoinsrm'
-d='plyamdkesx'
+# convert letters to uppercase
+letters = [i.upper() for i in letters]
 
-D2={}
-for letter1 in a:
-    for letter2 in b:
-        for letter3 in c:
-            for letter4 in d:
-                word=(letter1+letter2+letter3+letter4).upper()
-                if word in Dictionary:
-                    D2[word]=True
+# Load the dictionary file into a dictionary object
+def load_dict(path):
+    Dictionary={}
+    with open(path) as Dictionary_File:
+        for word in Dictionary_File:
+            # only look at words with the same number of letters as
+            # we have character dials
+            if len(word.strip()) == len(letters):
+                Dictionary[word.strip().upper()] = False
+    return Dictionary
 
-print(D2)
-while True:
-    x = input("Type a word and see if it can be spelled: ").upper()
-    if x in D2:
-        print(x in D2)
+# Recursively build every word from letter list and check against
+# dictionary
+def check_word(word_so_far, remaining_dials, word_dict):
+    # check completed word against dictionary
+    if len(remaining_dials) == 0:
+        if word_so_far in word_dict:
+            return [word_so_far]
+        else:
+            return []
+    # build all words with next set of letters and recurse
+    else:
+        wordlist = []
+        for c in remaining_dials[0]:
+            wordlist += check_word(word_so_far + c, remaining_dials[1:], word_dict)
+        return wordlist
+
+def main():
+    # load dictionary
+    word_dict = load_dict("Dictionary.txt")
+
+    # compare letters with dictionary
+    matching_words = check_word("", letters, word_dict)
+    print(matching_words)
+
+    # build dictionary from word list (cuz speed)
+    matching_words_dict = {i:None for i in matching_words}
+
+    # compare input against matched dictionary
+    while True:
+        x = input("Type a word and see if it can be spelled: ").upper()
+        print(x in matching_words_dict)
+
+if __name__ == '__main__':
+    main()
 
 
